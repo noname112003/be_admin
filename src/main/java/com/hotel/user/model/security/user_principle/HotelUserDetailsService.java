5e8142a1cd9dc394c945ee07f131c1538d4c842b
noname112003 <lvquang7112003@gmail.com>
2024-12-09 09:25:00 +0700

    update

--- src/main/java/com/hotel/user/model/security/user_principle/HotelUserDetailsService.java
+++ src/main/java/com/hotel/user/model/security/user_principle/HotelUserDetailsService.java
@@ -1,6 +1,7 @@
 package com.hotel.user.model.security.user_principle;
 
 import com.hotel.user.model.entity.User;
+import com.hotel.user.model.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class HotelUserDetailsService implements UserDetailsService {
     @Autowired
-    private com.hotel.user.repository.UserRepository userRepository;
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
